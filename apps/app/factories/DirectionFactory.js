6dc849062d13c547ea8359b326cecfe3270d327b
Mike Meadows <mmeadow33@gmail.com>
2016-08-10 14:39:42 -0500

    changed call method for weatherFactory, now its ng-mouseEnter

--- apps/app/factories/DirectionFactory.js
+++ apps/app/factories/DirectionFactory.js
@@ -9,11 +9,10 @@ app.factory("DirectionFactory", function($q, $http){
   var getDirectionLocation = {};
     getDirectionLocation.getZip = function(zip){
         return $q(function(resolve, reject){
-          $http.get(`https://maps.googleapis.com/maps/api/staticmap?center=Berkeley,CA&zoom=14&size=400x400&key=AIzaSyCnQ_QPnYxnH4JCxp62HRtQk2To8pUj_UQ
-
+          $http.get(`https://maps.googleapis.com/maps/api/directions/json?origin=Brooklyn&destination=Queens&mode=transit&key=AIzaSyBVDl4aptUgh5ObbFwDXkBx2ppTrM8JTU8
 `)
             .success(function(data){
-              // console.log("Directions Worked!!!!", data);
+              console.log("Directions Worked!!!!", data);
                 resolve(data);
             })
             .error(function(error){
