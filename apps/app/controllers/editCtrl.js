e272a24df8b3d2f3934ee7afecee6757ac5a11bc
Mike Meadows <mmeadow33@gmail.com>
2016-08-09 22:25:56 -0500

    fixed editCtrl.js

--- apps/app/controllers/editCtrl.js
+++ apps/app/controllers/editCtrl.js
@@ -21,21 +21,21 @@ app.controller("editCtrl", function($scope, LocationFactory, $routeParams, Fireb
     $scope.edited = true;
 }
 
-  $scope.items = [
-     {url: "http://static.panoramio.com/photos/large/43588000.jpg", name: "American Colloid Company"},
-     {url: "images/axiall aberdeen.jpg", name:"Axiall"},
-     {url: "images/Basf.jpg", name: "BASF"},
-     {url: "images/Continental.jpg", name:"Continental"},
-     {url: "images/Corning Diesel.jpg", name: "Corning"},
-     {url: "images/JZ tulsa.jpg", name: "John Zink"},
-     {url: "images/LeafRiver Energy Center Taylorsville, MS.jpg", name:"Leaf River"},
-     {url: "images/roxul-website_header_img.jpg", name:"Roxul"},
-     {url: "images/PSC Metals Chattanooga.jpg", name: "PSC Metals"},
-     {url: "images/Trojan Sandersville.jpg", name: "Trojan Battery"},
-     {url: "images/Willaims compression 515 wilkes-barre, PA.jpg", name: "William St. 515"},
-     {url: "images/Williams 240 NJ.jpg", name: "William St. 240"},
-     {url: "images/Williams 517 Benton PA.gif", name: "William St. 517"}
-   ]
+$scope.items = [
+   {url: "http://static.panoramio.com/photos/large/43588000.jpg", name: "American Colloid Company", zip: "36047"},
+   {url: "images/axiall aberdeen.jpg", name:"Axiall", zip: "39730"},
+   {url: "images/Basf.jpg", name: "BASF", zip: "44035"},
+   {url: "images/Continental.jpg", name:"Continental", zip: "29153"},
+   {url: "images/Corning Diesel.jpg", name: "Corning", zip: "14870"},
+   {url: "images/JZ tulsa.jpg", name: "John Zink", zip: "74116"},
+   {url: "images/LeafRiver Energy Center Taylorsville, MS.jpg", name:"Leaf River", zip: "39422"},
+   {url: "images/roxul-website_header_img.jpg", name:"Roxul", zip: "38611"},
+   {url: "images/PSC Metals Chattanooga.jpg", name: "PSC Metals", zip: "37405"},
+   {url: "images/Trojan Sandersville.jpg", name: "Trojan Battery", zip: "31082"},
+   {url: "images/Willaims compression 515 wilkes-barre, PA.jpg", name: "William St. 515", zip: "18705"},
+   {url: "images/Williams 240 NJ.jpg", name: "William St. 240", zip: "07073"},
+   {url: "images/Williams 517 Benton PA.gif", name: "William St. 517 ", zip: "17814"}
+ ]
 
 
    $scope.selectedItem;
@@ -59,14 +59,10 @@ let currentUser = AuthFactory.getUser("currentUser");
       location: $scope.location,
       emissions: $scope.emissions,
       uid: currentUser,
-      image: $scope.image
+      image: $scope.image,
+      zip: $scope.zip
     }
-
 console.log(newTask);
-
-
-
-///////////////////////Something in here is redifining the object Id//////////////////////////
 
 let thingy = $routeParams;
 thingy = thingy.locationId;
