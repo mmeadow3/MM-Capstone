3c8c6933424100340a7227cd6ddea2fa4758ab4b
Mike Meadows <mmeadow33@gmail.com>
2016-08-09 22:08:54 -0500

    map image is now working, cards are looking like final products

--- apps/app/controllers/Locations.js
+++ apps/app/controllers/Locations.js
@@ -6,40 +6,27 @@ app.controller('Locations', function($scope, LocationFactory, weatherFactory, Di
       LocationFactory.getLocationList()
     .then(function(locationCollection){
       $scope.locations = locationCollection;
-
-    // for (var i = 0; i < $scope.locations.length; i++){
-    // weatherFactory.getWeatherZip.getZip($scope.locations[i].zip)
-    // .then(function(data){
-    // console.log(data.location.city)
-    // $scope.weather = data.location.city
-    // })
-    // }
     })
   };
 
 
 $scope.APIcall = function (zip) {
  weatherFactory.getWeatherZip.getZip(zip.zip)
-.then(function(data){
-						zip.theWeather = data.forecast.simpleforecast;
+  .then(function(data){
+		zip.theWeather = data.forecast.simpleforecast;
   });
 }
 
 $scope.directionCall = function (zip) {
- DirectionFactory.getDirectionLocation.getZip(zip.zip)
-}
-
+    DirectionFactory.getDirectionLocation.getZip(zip.zip)
+  }
 
 getLocation();
 
-  $scope.remove = function (location) {
-    console.log(location)
-		LocationFactory.deleteLocation(location)
-		.then(function () {
-			getLocation()
-			});
-		}
-
-
-
+$scope.remove = function (location) {
+	LocationFactory.deleteLocation(location)
+	 .then(function () {
+	    getLocation()
+	  });
+}
 });
--- apps/app/factories/DirectionFactory.js
+++ apps/app/factories/DirectionFactory.js
@@ -9,10 +9,11 @@ app.factory("DirectionFactory", function($q, $http){
   var getDirectionLocation = {};
     getDirectionLocation.getZip = function(zip){
         return $q(function(resolve, reject){
-          $http.get(`https://maps.googleapis.com/maps/api/geocode/json?address=` + zip + `&key=AIzaSyCnQ_QPnYxnH4JCxp62HRtQk2To8pUj_UQ
+          $http.get(`https://maps.googleapis.com/maps/api/staticmap?center=Berkeley,CA&zoom=14&size=400x400&key=AIzaSyCnQ_QPnYxnH4JCxp62HRtQk2To8pUj_UQ
+
 `)
             .success(function(data){
-              console.log("Directions Worked!!!!", data);
+              // console.log("Directions Worked!!!!", data);
                 resolve(data);
             })
             .error(function(error){
